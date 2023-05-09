b9294e3bc731dcb71221ea2095b67166ab7d4337
Cody Hilborn <codyhilborn11@gmail.com>
2023-05-09 15:37:37 -0400

    Add interface example

---
+++ types/types.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function printName(stark) {
+    console.log(stark.name);
+}
+printName({ name: 'Eddard' });
+printName({ name: "Joe" });
